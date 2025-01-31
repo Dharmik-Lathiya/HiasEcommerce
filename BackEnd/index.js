cf2334403952091d0c17881d8a61d9252c2b2d43
jadejashaktisinh <bca2023shaktisinh1892@tnraocollege.org>
2025-01-31 22:09:59 +0530

    cors

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -13,7 +13,18 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ limit: '50mb',extended:false, parameterLimit: 50000}))
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use('/',route);
-
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
+    next();
+  });
 app.listen(3000,()=>{
 
 })
